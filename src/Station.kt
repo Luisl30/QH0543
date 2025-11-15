815d6fede4a8adc11cc6a294a8d5c00286ff566f
luisl30 <igotta30@gmail.com>
2025-11-15 15:34:35 +0000

    change the single and return price to var instead of val

--- src/Station.kt
+++ src/Station.kt
@@ -1,7 +1,7 @@
 data class Station (
     val name: String,
-    val singlePrice: Double,
-    val returnPrice: Double,
+    var singlePrice: Double,
+    var returnPrice: Double,
     /* var salesCount: Int = 0,*/
     var takings: Double = 0.0
 ){}
