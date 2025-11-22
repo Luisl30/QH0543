113857bb2af17796cece229d103872763fc7f4c4
KrzysztofDrezek <0Drezk79@solent.ac.uk>
2025-11-22 10:18:49 +0000

    Add hard-coded admin and user list

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,11 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+val users = listOf(
+    User(username = "admin", password = "admin123", isAdmin = true),
+    User(username = "user", password = "user123", isAdmin = false)
+)
+
 fun main() {
     val name = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
